cdbb93d668d3135484187980b3fd322bd2127a2d
Laptop-GB <giuseppe.bettioni@gmail.com>
2024-02-21 19:20:02 +0100

    21-02

---
+++ src/main/java/esteoria/teoria06.java
@@ -0,0 +1,21 @@
+package esteoria;
+
+import javax.swing.*;
+
+public class teoria06 {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Inserire numero"));
+        String check = "";
+        int temp = numero;
+        while (temp > 0){
+            check += temp%10;
+            temp /=10;
+        }
+        temp = Integer.parseInt(check);
+        if ( numero == temp ){
+            JOptionPane.showMessageDialog(null,"È palindromo");
+        } else {
+            JOptionPane.showMessageDialog(null,"Non è palindromo");
+        }
+    }
+}
