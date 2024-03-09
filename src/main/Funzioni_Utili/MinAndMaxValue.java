e242de9f594d0b3655592ebf356f0f6be15a203b
giuseppe <giuseppe.bettioni@gmail.com>
2024-03-09 14:59:39 +0100

    09-03

---
+++ src/main/Funzioni_Utili/MinAndMaxValue.java
@@ -0,0 +1,8 @@
+package main.Funzioni_Utili;
+
+public class MinAndMaxValue {
+    public static void main(String[] args) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+    }
+}
